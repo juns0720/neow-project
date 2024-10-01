1586e5edbaa9cef1153a1427983804c181ad669c
junseo Lee <74139368+juns0720@users.noreply.github.com>
2024-10-01 19:13:31 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/NeowProject/NeowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.NeowProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeowProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeowProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NeowProject/NeowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NeowProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
