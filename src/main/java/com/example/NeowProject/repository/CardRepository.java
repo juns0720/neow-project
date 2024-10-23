19a92d98b038f871133d3d2c51cc1da9f74feaa4
junseo Lee <juns0720@naver.com>
2024-10-23 22:08:32 +0900

    feat: "Card Service 기능 및 테스트 추가"

--- src/main/java/com/example/NeowProject/repository/CardRepository.java
+++ src/main/java/com/example/NeowProject/repository/CardRepository.java
@@ -4,13 +4,15 @@ import com.example.NeowProject.domain.Card;
 import com.example.NeowProject.domain.CardType;
 import com.example.NeowProject.domain.Color;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 
-    List<Card> findCardsByName(String name);
 
     List<Card> findCardsByCardType(CardType cardType);
 
---
+++ src/test/java/com/example/NeowProject/service/CardServiceTest.java
@@ -0,0 +1,82 @@
+package com.example.NeowProject.service;
+
+import com.example.NeowProject.domain.Card;
+import com.example.NeowProject.domain.CardType;
+import com.example.NeowProject.domain.Color;
+import com.example.NeowProject.domain.Rarity;
+import com.example.NeowProject.repository.CardRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CardServiceTest {
+    @Autowired
+    private CardRepository cardRepository;
+    @Autowired
+    private CardService cardService;
+
+    @Test
+    void findById() {
+        Card card = createCard("박치기", Color.RED, 2, CardType.ATTACK, Rarity.COMMON);
+
+        Long savedId = cardService.save(card);
+
+        assertEquals(savedId, cardService.findById(savedId).getId());
+    }
+
+    @Test
+    void findAll() {
+        Card card1 = createCard("박치기", Color.RED, 2, CardType.ATTACK, Rarity.COMMON);
+        Card card2 = createCard("던지기", Color.RED, 2, CardType.ATTACK, Rarity.COMMON);
+
+        cardService.save(card1);
+        cardService.save(card2);
+
+        assertEquals(cardService.findAll().size(), 2);
+
+    }
+
+    @Test
+    void findCardsByCardType() {
+        Card card1 = createCard("박치기", Color.RED, 2, CardType.ATTACK, Rarity.COMMON);
+        Card card2 = createCard("던지기", Color.RED, 2, CardType.POWER, Rarity.COMMON);
+        Card card3 = createCard("잡기", Color.RED, 2, CardType.POWER, Rarity.COMMON);
+
+        cardService.save(card1);
+        cardService.save(card2);
+        cardService.save(card3);
+
+        assertEquals(cardService.findCardsByCardType(CardType.POWER).size(), 2);
+
+    }
+
+    @Test
+    void findCardsByColor() {
+        Card card1 = createCard("박치기", Color.RED, 2, CardType.ATTACK, Rarity.COMMON);
+        Card card2 = createCard("던지기", Color.BLACK, 2, CardType.POWER, Rarity.COMMON);
+        Card card3 = createCard("잡기", Color.RED, 2, CardType.POWER, Rarity.COMMON);
+
+        cardService.save(card1);
+        cardService.save(card2);
+        cardService.save(card3);
+
+        assertEquals(cardService.findCardsByColor(Color.BLACK).size(), 1);
+        assertEquals(cardService.findCardsByColor(Color.RED).size(), 2);
+    }
+
+
+    static Card createCard(String name, Color color, int cost, CardType cardType, Rarity rarity) {
+        Card card = new Card();
+        card.setName(name);
+        card.setColor(color);
+        card.setCost(cost);
+        card.setCardType(cardType);
+        card.setRarity(rarity);
+        return card;
+    }
+}
