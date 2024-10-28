b60aaea2187c783d46be43177d659e38b84e7f33
songhj <427shg@gmail.com>
2024-10-28 15:58:21 +0900

    feat : 게임 삭제 관련 에러 코드 추가

--- src/main/java/com/example/NeowProject/exception/ErrorCode.java
+++ src/main/java/com/example/NeowProject/exception/ErrorCode.java
@@ -10,9 +10,11 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND),
     FILE_NOT_FOUND("JSON file not found", HttpStatus.NOT_FOUND),
     RELIC_NOT_FOUND("Relic not found", HttpStatus.NOT_FOUND),
+    GAME_NOT_FOUND("Game not found", HttpStatus.NOT_FOUND),
 
     FILE_SAVE_FAILED("Failed to save JSON file", HttpStatus.INTERNAL_SERVER_ERROR),
-    FILE_READ_FAILED("Failed to read JSON file", HttpStatus.INTERNAL_SERVER_ERROR);
+    FILE_READ_FAILED("Failed to read JSON file", HttpStatus.INTERNAL_SERVER_ERROR),
+    FILE_DELETE_FAILED("Failed to delete JSON file", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String message;
     private final HttpStatus status;
