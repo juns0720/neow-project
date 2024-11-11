2196791f268405639faec804682c45de6f198501
songhj <427shg@gmail.com>
2024-11-11 21:23:00 +0900

    fix: 안 쓰는 import 문 삭제

--- src/main/java/com/example/NeowProject/controller/StatisticsController.java
+++ src/main/java/com/example/NeowProject/controller/StatisticsController.java
@@ -7,7 +7,6 @@ import com.example.NeowProject.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
