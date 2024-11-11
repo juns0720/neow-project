f2ab79d61db7eebcec9b3bee317f7495c6f1cd45
songhj <427shg@gmail.com>
2024-11-11 20:46:57 +0900

    feat : StatisticsController 작성

---
+++ src/main/java/com/example/NeowProject/controller/StatisticsController.java
@@ -0,0 +1,50 @@
+package com.example.NeowProject.controller;
+
+import com.example.NeowProject.domain.CharacterType;
+import com.example.NeowProject.domain.Color;
+import com.example.NeowProject.dto.response.CardDataResponse;
+import com.example.NeowProject.dto.response.CharacterDataResponse;
+import com.example.NeowProject.dto.response.EnemyDataResponse;
+import com.example.NeowProject.dto.response.RelicDataResponse;
+import com.example.NeowProject.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/global")
+@RequiredArgsConstructor
+public class StatisticsController {
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/character")
+    public ResponseEntity<?> getCharacterData(@RequestParam("character") String character) {
+        CharacterDataResponse characterData = statisticsService.getCharacterData(CharacterType.valueOf(character));
+        return ResponseEntity.status(HttpStatus.OK).body(characterData);
+    }
+
+    @GetMapping("/card")
+    public ResponseEntity<?> getCardData(@RequestParam("color") String color) {
+        List<CardDataResponse> cardDataByColor = statisticsService.getCardDataByColor(Color.valueOf(color));
+        return ResponseEntity.status(HttpStatus.OK).body(cardDataByColor);
+    }
+
+    @GetMapping("/relic")
+    public ResponseEntity<?> getRelicData() {
+        RelicDataResponse relicData = statisticsService.getRelicData();
+        return ResponseEntity.status(HttpStatus.OK).body(relicData);
+    }
+
+    @GetMapping("/death-rate")
+    public ResponseEntity<?> getDeathData() {
+        List<EnemyDataResponse> enemyData = statisticsService.getEnemyData();
+        return ResponseEntity.status(HttpStatus.OK).body(enemyData);
+    }
+}
